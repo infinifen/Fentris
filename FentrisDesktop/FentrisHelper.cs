ee28407fccf24cdff6d793a9d4b5117c5fbbe996
infinifen <48841404+infinifen@users.noreply.github.com>
2022-11-30 01:11:17 +0100

    we're rendering the board babyyyyyyy

---
+++ FentrisDesktop/FentrisHelper.cs
@@ -0,0 +1,25 @@
+﻿using FentrisDesktop.Board;
+using Microsoft.Xna.Framework;
+
+namespace FentrisDesktop;
+
+public static class FentrisHelper
+{
+    public static Color Color(this BlockKind bk)
+    {
+        return bk switch
+        {
+            BlockKind.Blue => Microsoft.Xna.Framework.Color.Blue,
+            BlockKind.Bone => Microsoft.Xna.Framework.Color.Gray,
+            BlockKind.Clear => Microsoft.Xna.Framework.Color.Transparent,
+            BlockKind.Cyan => Microsoft.Xna.Framework.Color.Cyan,
+            BlockKind.Garbage => Microsoft.Xna.Framework.Color.DarkGray,
+            BlockKind.Green => Microsoft.Xna.Framework.Color.Green,
+            BlockKind.Magenta => Microsoft.Xna.Framework.Color.Magenta,
+            BlockKind.Orange => Microsoft.Xna.Framework.Color.Orange,
+            BlockKind.Red => Microsoft.Xna.Framework.Color.Red,
+            BlockKind.Yellow => Microsoft.Xna.Framework.Color.Yellow,
+            BlockKind.OutOfBounds => Microsoft.Xna.Framework.Color.Transparent
+        };
+    }
+}
--- FentrisDesktop/Gamemode.cs
+++ FentrisDesktop/Gamemode.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
 using MonoGame.Extended.Screens;
 
 namespace FentrisDesktop.Board;
@@ -15,10 +17,13 @@ public class Gamemode : GameScreen
     public Queue<PieceShape> Next;
     public readonly int NextAmount;
     protected RenderTarget2D BoardRenderTarget;
+    protected SpriteBatch SpriteBatch;
+    protected int BoardBorderThickness = 5;
     public int Gravity => 0; // gravity ticks per frame
     public IRandomizer Randomizer;
 
     public int frame = 0;
+
     // rotation system goes here later
 
     public Gamemode(FentrisGame game) : base(game)
@@ -28,7 +33,13 @@ public class Gamemode : GameScreen
         Randomizer = new TestRandomizer();
         Next = new(Enumerable.Range(0, NextAmount).Select(_ => Randomizer.GenerateNext()));
         BoardRenderTarget =
-            new RenderTarget2D(game.GraphicsDevice, 320, 1280, false, SurfaceFormat.Alpha8, DepthFormat.None);
+            new RenderTarget2D(game.GraphicsDevice, 640 + BoardBorderThickness * 2, 1280 + BoardBorderThickness * 2,
+                false, SurfaceFormat.Alpha8, DepthFormat.None);
+    }
+
+    public override void LoadContent()
+    {
+        SpriteBatch = new SpriteBatch(Game.GraphicsDevice);
     }
 
     public void CycleNext()
@@ -58,8 +69,9 @@ public class Gamemode : GameScreen
         var newY = (ActivePiece.SubY + gravity) / 256;
         for (int row = ActivePiece.Y; row < newY; row++)
         {
-            if (!Board.CollidePiece(ActivePiece, ActivePiece.X, row)) continue; // no collision after going down a row, magnificent
-            
+            if (!Board.CollidePiece(ActivePiece, ActivePiece.X, row))
+                continue; // no collision after going down a row, magnificent
+
             // the piece got shoved into the stack due to gravity, last good position was one above current
             ActivePiece.Y = row - 1;
             break;
@@ -72,7 +84,7 @@ public class Gamemode : GameScreen
         // rotation system stuff will go here later
         ActivePiece.Rotation += direction;
         if (!Board.CollidePiece(ActivePiece)) return; // successful natural rotation
-        
+
         // natural rotation blocked, for now just fail the rotation
         ActivePiece.Rotation = oldRotation;
         return;
@@ -80,6 +92,26 @@ public class Gamemode : GameScreen
 
     public override void Update(GameTime gameTime)
     {
+        Board[0, 0] = new Block(BlockKind.Bone);
+        Board[1, 1] = new Block(BlockKind.Bone);
+        Board[2, 2] = new Block(BlockKind.Bone);
+        Board[3, 3] = new Block(BlockKind.Bone);
+        Board[4, 4] = new Block(BlockKind.Bone);
+        Board[5, 5] = new Block(BlockKind.Bone);
+        Board[6, 6] = new Block(BlockKind.Bone);
+        Board[7, 7] = new Block(BlockKind.Bone);
+        Board[8, 8] = new Block(BlockKind.Bone);
+        Board[9, 9] = new Block(BlockKind.Bone);
+        Board[8, 10] = new Block(BlockKind.Bone);
+        Board[7, 11] = new Block(BlockKind.Bone);
+        Board[6, 12] = new Block(BlockKind.Bone);
+        Board[5, 13] = new Block(BlockKind.Bone);
+        Board[4, 14] = new Block(BlockKind.Bone);
+        Board[3, 15] = new Block(BlockKind.Bone);
+        Board[2, 16] = new Block(BlockKind.Bone);
+        Board[1, 17] = new Block(BlockKind.Bone);
+        Board[0, 18] = new Block(BlockKind.Bone);
+        Board[1, 19] = new Block(BlockKind.Bone);
     }
 
     public override void Draw(GameTime gameTime)
@@ -89,19 +121,42 @@ public class Gamemode : GameScreen
 
     private void DrawBoard()
     {
-        var (rx, ry) = (0, 0); // replace this with actual centering logic later
+        var margin = 40;
+        var boardH = Game.H - margin * 2;
+        var boardW = boardH / 2;
+
+        var rx = Game.W / 2 - boardW / 2;
+        var ry = margin;
+
         Game.GraphicsDevice.SetRenderTarget(BoardRenderTarget);
         Game.GraphicsDevice.Clear(Color.Transparent);
 
-        // board render target is 320x1280, so a single mino is 32x32
+        SpriteBatch.Begin();
+        // board render target minus border is 640x1280, so a single mino is 64x64
         for (int y = 0; y < Board.board.GetLength(1); y++)
         {
-            for (int x = 0; x < Board.board.GetLength(0); y++)
+            for (int x = 0; x < Board.board.GetLength(0); x++)
             {
                 var block = Board[x, y];
-                
+                if (block.kind != BlockKind.Clear)
+                {
+                    SpriteBatch.FillRectangle(new Vector2(x * 64 + BoardBorderThickness, y * 64 + BoardBorderThickness),
+                        new Size2(64, 64),
+                        block.kind.Color());
+                }
             }
         }
+        
+        SpriteBatch.DrawRectangle(0, 0, BoardRenderTarget.Width,
+            BoardRenderTarget.Height, Color.White, BoardBorderThickness);
+
+        SpriteBatch.End();
+        Game.GraphicsDevice.SetRenderTarget(null);
+
+        // draw board to screen
+        SpriteBatch.Begin();
+        SpriteBatch.Draw(BoardRenderTarget, new Rectangle(rx, ry, boardW, boardH), Color.White);
+        SpriteBatch.End();
     }
 
     public override void UnloadContent()
