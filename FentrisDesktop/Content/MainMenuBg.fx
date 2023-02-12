101a06537485022b5edb030b7dd10251ec827f07
infinifen <48841404+infinifen@users.noreply.github.com>
2023-02-12 21:48:46 +0100

    main menu screen now has a background :3

---
+++ FentrisDesktop/Content/MainMenuBg.fx
@@ -0,0 +1,39 @@
+﻿#if OPENGL
+	#define SV_POSITION POSITION
+	#define VS_SHADERMODEL vs_3_0
+	#define PS_SHADERMODEL ps_3_0
+#else
+	#define VS_SHADERMODEL vs_4_0_level_9_1
+	#define PS_SHADERMODEL ps_4_0_level_9_1
+#endif
+
+Texture2D SpriteTexture;
+float4 tint;
+float time;
+
+sampler2D SpriteTextureSampler = sampler_state
+{
+	Texture = <SpriteTexture>;
+};
+
+struct VertexShaderOutput
+{
+	float4 Position : SV_POSITION;
+	float4 Color : COLOR0;
+	float2 TextureCoordinates : TEXCOORD0;
+};
+
+float4 MainPS(VertexShaderOutput input) : COLOR
+{
+    float4 col = tex2D(SpriteTextureSampler,input.TextureCoordinates);
+    col.rgb *= 0.6 + 0.4 * pow(sin(1.7 * time + 6.2 * input.TextureCoordinates.x - 2.4 * input.TextureCoordinates.y), 2.4);
+	return col * input.Color * tint;
+}
+
+technique SpriteDrawing
+{
+	pass P0
+	{
+		PixelShader = compile PS_SHADERMODEL MainPS();
+	}
+};
--- FentrisDesktop/StartMenuScreen.cs
+++ FentrisDesktop/StartMenuScreen.cs
@@ -4,8 +4,10 @@ using FontStashSharp;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
 using MonoGame.Extended.Input;
 using MonoGame.Extended.Screens;
+using MonoGame.Extended.Tweening;
 using static FentrisDesktop.FentrisHelper;
 
 
@@ -15,20 +17,16 @@ public class StartMenuScreen : GameScreen
 {
     public static readonly MenuEntry[] Entries = new[]
     {
-        new MenuEntry("Normal mode", "The classic beginner experience", game =>
-        {
-            game.LoadGamemode(new Gamemode.Gamemode());
-        }),
-        
-        new MenuEntry("Apocalypse", "Delay the descent into insanity.", game =>
-        {
-            game.LoadGamemode(new ApocalypseGamemode());
-        }),
-        
-        new MenuEntry("Exit", "See you next time.", game =>
-        {
-            game.Exit();
-        })
+        new MenuEntry("Normal mode", "The classic beginner experience",
+            game => { game.LoadGamemode(new Gamemode.Gamemode()); },
+            new Color(0.1f, 0.73f, 0.2f, 1f)),
+
+        new MenuEntry("Apocalypse", "Delay the descent into insanity.",
+            game => { game.LoadGamemode(new ApocalypseGamemode()); },
+            new Color(0.82f, 0.23f, 0.1f, 1f)),
+
+        new MenuEntry("Exit", "See you next time.", game => { game.Exit(); },
+            new Color(1f, 1f, 1f, 1f))
     };
 
     private int _menuIdx = 0;
@@ -38,7 +36,9 @@ public class StartMenuScreen : GameScreen
     private DynamicSpriteFont _menuItemFont;
     private DynamicSpriteFont _taglineFont;
     private SpriteBatch _spriteBatch;
-    
+    private Texture2D _keysBackground;
+    private Effect _backgroundShader;
+
     private int BaseFontSize
     {
         get
@@ -48,7 +48,7 @@ public class StartMenuScreen : GameScreen
             return Math.Max(fontSize, 12);
         }
     }
-    
+
     public StartMenuScreen(FentrisGame game) : base(game)
     {
         this._game = game;
@@ -58,11 +58,10 @@ public class StartMenuScreen : GameScreen
     {
         ReloadFont();
         _spriteBatch = new SpriteBatch(_game.GraphicsDevice);
+        _keysBackground = Content.Load<Texture2D>("tpiece_4k");
+        _backgroundShader = Content.Load<Effect>("MainMenuBg");
 
-        Game.Window.ClientSizeChanged += (sender, args) =>
-        {
-            ReloadFont();
-        };
+        Game.Window.ClientSizeChanged += (sender, args) => { ReloadFont(); };
     }
 
     private void ReloadFont()
@@ -74,10 +73,9 @@ public class StartMenuScreen : GameScreen
     }
 
 
-
     public override void UnloadContent()
     {
-       _spriteBatch.Dispose();
+        _spriteBatch.Dispose();
     }
 
     public override void Update(GameTime gameTime)
@@ -88,12 +86,12 @@ public class StartMenuScreen : GameScreen
         {
             _menuIdx++;
         }
-        
+
         if (state.WasKeyJustUp(Keys.Up))
         {
             _menuIdx--;
         }
-        
+
         if (state.WasKeyJustUp(Keys.Enter))
         {
             CurrentMenuItem.Action(_game);
@@ -102,9 +100,18 @@ public class StartMenuScreen : GameScreen
 
     public override void Draw(GameTime gameTime)
     {
+        _spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend);
+        _backgroundShader.Parameters["tint"].SetValue(CurrentMenuItem.Tint.ToVector4());
+        _backgroundShader.Parameters["time"].SetValue((float) gameTime.TotalGameTime.TotalSeconds);
+        _backgroundShader.Techniques["SpriteDrawing"].Passes[0].Apply();
+        var (bgPos, bgScale) = ComputeBackgroundScale(_keysBackground);
+        _spriteBatch.Draw(_keysBackground, bgPos, scale: bgScale, sourceRectangle: null, color: Color.White,
+            origin: Vector2.Zero, effects: SpriteEffects.None, rotation: 0f, layerDepth: 0f);
+        _spriteBatch.End();
+
         _spriteBatch.Begin();
         var y = _game.H * 0.4f;
-        for(int i = 0; i < Entries.Length; i++)
+        for (int i = 0; i < Entries.Length; i++)
         {
             var item = Entries[i];
             var measure = _menuItemFont.MeasureString(item.Title);
@@ -114,26 +121,44 @@ public class StartMenuScreen : GameScreen
             y += measure.Y * 2;
         }
 
-        var taglineMeasure = _menuItemFont.MeasureString(CurrentMenuItem.Tagline);
-        _spriteBatch.DrawString(_menuItemFont, CurrentMenuItem.Tagline,
+        var taglineMeasure = _taglineFont.MeasureString(CurrentMenuItem.Tagline);
+        _spriteBatch.DrawString(_taglineFont, CurrentMenuItem.Tagline,
             new Vector2(20, _game.H - taglineMeasure.Y - 20), Color.White);
-        
+
 
         _spriteBatch.End();
+    }
+
+    private (Vector2, float) ComputeBackgroundScale(Texture2D tex)
+    {
+        var xB = tex.Width;
+        var yB = tex.Height;
+
+        var xR = _game.W / (float)xB;
+        var yR = _game.H / (float)yB;
+        var scale = Math.Max(xR, yR);
+
+        var excessX = xB * scale - _game.W;
+        var excessY = yB * scale - _game.H;
+
+        return (new Vector2(-excessX / 2, -excessY / 2), scale);
     }
 }
 
 public struct MenuEntry
 {
-    public delegate void MenuAction(FentrisGame game); 
+    public delegate void MenuAction(FentrisGame game);
+
     public string Title;
     public string Tagline;
     public MenuAction Action;
+    public Color Tint;
 
-    public MenuEntry(string title, string tagline, MenuAction action)
+    public MenuEntry(string title, string tagline, MenuAction action, Color tint)
     {
         Title = title;
         Tagline = tagline;
         Action = action;
+        Tint = tint;
     }
 }
