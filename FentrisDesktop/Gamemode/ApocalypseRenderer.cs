5c8d2ceb36ef36dcfc32fbd07d3a4869e924ac80
infinifen <48841404+infinifen@users.noreply.github.com>
2023-04-04 19:42:55 +0200

    migrate remaining gamemodes to new scoring rendering

--- FentrisDesktop/Gamemode/ApocalypseRenderer.cs
+++ FentrisDesktop/Gamemode/ApocalypseRenderer.cs
@@ -21,8 +21,22 @@ public class ApocalypseRenderer : GamemodeRenderer
     public override void Draw(GameTime gameTime)
     {
         base.Draw(gameTime);
+    }
+
+    protected override void DrawScoring()
+    {
+        base.DrawScoring();
         SpriteBatch.Begin();
-        SpriteBatch.DrawString(Game.LargeFont, $"SPEED: {Mode.SpeedLv:F0}", new Vector2(0, 320), Color.Red);
+        var boardRect = CalculateBoardRect();
+        var speedInfo = "Speed";
+        var speedStr = $"{Mode.SpeedLv:F0}";
+
+        (var speedPos, var speedInfoPos) = FentrisHelper.GetScoringLayout(
+            boardRect, 0.6f, 10, 10, speedInfo, Game.MediumFont, speedStr, Game.LargeFont
+        );
+
+        SpriteBatch.DrawString(Game.LargeFont, speedStr, speedPos, Color.Red);
+        SpriteBatch.DrawString(Game.MediumFont, speedInfo, speedInfoPos, Color.White);
         SpriteBatch.End();
     }
 }
--- FentrisDesktop/Gamemode/NormalRenderer.cs
+++ FentrisDesktop/Gamemode/NormalRenderer.cs
@@ -21,9 +21,31 @@ public class NormalRenderer : GamemodeRenderer
     public override void Draw(GameTime gameTime)
     {
         base.Draw(gameTime);
+    }
+
+    protected override void DrawScoring()
+    {
+        base.DrawScoring();
+        
         SpriteBatch.Begin();
-        SpriteBatch.DrawString(DebugFont, $"x{Mode.Combo:F2}", new Vector2(0, 300), Color.Aqua);
-        SpriteBatch.DrawString(Game.LargeFont, Mode.Score.ToString(), new Vector2(0, 320), Color.Red);
+        var boardRect = CalculateBoardRect();
+        var multInfo = "";
+        var multStr = $"x{Mode.Combo:F2}";
+        var scoreInfo = "Score";
+        var scoreStr = Mode.Score.ToString();
+
+        (var multPos, var multInfoPos) = FentrisHelper.GetScoringLayout(
+            boardRect, 0.55f, 10, 10, multInfo, Game.MediumFont, multStr, Game.MediumFont
+        );
+        
+        (var scorePos, var scoreInfoPos) = FentrisHelper.GetScoringLayout(
+            boardRect, 0.6f, 10, 10, scoreInfo, Game.MediumFont, scoreStr, Game.LargeFont
+        );
+
+        SpriteBatch.DrawString(Game.MediumFont, multStr, multPos, Color.Aqua);
+        SpriteBatch.DrawString(Game.LargeFont, scoreStr, scorePos, Color.White);
+        SpriteBatch.DrawString(Game.MediumFont, scoreInfo, scoreInfoPos, Color.White);
+        SpriteBatch.DrawString(Game.MediumFont, multInfo, multInfoPos, Color.White);
         SpriteBatch.End();
     }
 }
