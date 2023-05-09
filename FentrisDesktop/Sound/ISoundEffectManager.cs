70f83204d0c604f2b31dec83901e4bbd603520ea
infinifen <48841404+infinifen@users.noreply.github.com>
2023-05-09 18:47:07 +0200

    add sound effect files

---
+++ FentrisDesktop/Sound/ISoundEffectManager.cs
@@ -0,0 +1,6 @@
+namespace FentrisDesktop.Sound;
+
+public interface ISoundEffectManager
+{
+    public void PlaySound(SoundEffect se);
+}
---
+++ FentrisDesktop/Sound/SoundEffect.cs
@@ -0,0 +1,8 @@
+namespace FentrisDesktop.Sound;
+
+public enum SoundEffect
+{
+    Move,
+    Drop,
+    Unlock,
+}
